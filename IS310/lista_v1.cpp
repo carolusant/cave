91cf8161f7f6ded0fe6b2192794541d0ec2f3ad0
Carlos'Gz <54749165+carolusant@users.noreply.github.com>
2020-02-10 13:26:45 -0600

    Update lista_v1.cpp

--- IS310/lista_v1.cpp
+++ IS310/lista_v1.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+using name space std;
 
+class Nodo {
+public:
+    Nodo *enlace;
+    int datos;
+    Nodo (){
+        enlace = NULL;
+        datos=0;
+    }
+};
+
+
+int main (){
+
+    cout<<"Hey ... "<<endl;
+    system("pause");
+    return 0;
+}
