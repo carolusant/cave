e57436c98c8e540ba8ed21f931742b2f6d6863b0
Carlos'Gz <54749165+carolusant@users.noreply.github.com>
2020-03-03 08:57:57 -0600

    Primer lab de ecuaciones

---
+++ Lab_Ecua/1lab.m
@@ -0,0 +1,41 @@
+##Primer labotorio de ecuaciones
+##3*(45+6)
+##(2.5+3i)*i
+
+va=2.5;
+va2=1.5; 
+va3=va+va2;
+printf("Suma de variables %f \n ",va3);
+
+##va-va2;
+##va*va2;
+
+
+#Ejercicio de exponecial
+
+base=5;
+exp=4;
+printf("Exponecial : %f \n",base^exp ); 
+
+
+
+##Ejercicio 1
+
+x=2.31;
+f= x^2 +sqrt(sin(x)) ;
+printf("F(x) evaluada en 2.31 %f \n ",f);
+
+##Ejercicio 2
+##Evaluarla en 20 puntos de [0,2] 
+
+y=0.1;
+
+##cos(2y) -3y^3 +1
+
+
+##Vectores y matrices
+
+
+A= [2 6  8; 3 54 5]
+
+m=[ 1 3 5 6; 6 3 6 3 ; 3 5 6 4] 
